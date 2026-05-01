63580abb39e23f973e1ee20f6ccee0abc4597794
cleftin <cleftin@naver.com>
2026-05-01 14:30:31 +0900

    mcp_server

---
+++ mcp_server.py
@@ -0,0 +1,15 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+# Add an addition tool
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+# Run with streamable HTTP transport
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
